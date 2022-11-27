8d444d677b3512badb78c98d25ea2feaa488e98b
yalem123 <yalembrhankelayneh@gmail.com>
2022-11-27 18:28:59 +0300

    edit js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
-    let checkedAmenities = {};
-    $(document).on('change', "input[type='checkbox']", function () {
-      if (this.checked) {
-        checkedAmenities[$(this).data('id')] = $(this).data('name');
+$(document).ready(function(){
+    let selected = []
+    $('input[type=checkbox]').change(function () {
+      if ($(this).prop('checked')) {
+        selected.push($(this).attr('data-name'))
       } else {
-        delete checkedAmenities[$(this).data('id')];
+        let unchecked = $(this).attr('data-name')
+        let idx = selected.indexOf(unchecked)
+        selected.splice(idx, 1)
       }
-      let lst = Object.values(checkedAmenities);
-      if (lst.length > 0) {
-        $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
-      } else {
-        $('div.amenities > h4').html('&nbsp;');
-      }
-    });
-   });
+    
+    if (selected.length !== 0) {
+      let seleced_in_txt = selected.join(", ")
+      $("#selected").text(seleced_in_txt)
+    }
+  })
+  })
